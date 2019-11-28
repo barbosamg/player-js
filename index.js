dd9fbcccbdbed943eab12c3cfb61c8c5acac3397
barbosamg <mateus_gabriel96@outlook.com>
2019-11-27 21:49:38 -0300

    alteração de cor no titulo

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 window.addEventListener('load', () => {
     player.start();
+    alterarCorTituloCard();
 });
+
+function alterarCorTituloCard(){
+    let tituloCard = document.querySelector('#player h4');
+    var cores = ['red', 'blue', 'black', 'green', 'yellow', 'orange', 'purple', 'skyblue'];
+    var contador = 0;
+    setInterval(() => {
+        tituloCard.style.color = cores[contador];
+        contador++;
+        if (contador > cores.length) {
+            contador = 0;
+        }
+    }, 3500);
+}
